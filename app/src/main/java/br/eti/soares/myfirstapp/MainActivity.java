2d803a222c918917451947ebb3e84f1cd573dea0
Fellipe Soares <soares.fellipe@outlook.com>
2014-12-24 11:32:22 -0200

    Criação de Activity para Visualizar Mensagem

--- app/src/main/java/br/eti/soares/myfirstapp/MainActivity.java
+++ app/src/main/java/br/eti/soares/myfirstapp/MainActivity.java
@@ -1,12 +1,17 @@
 package br.eti.soares.myfirstapp;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
+
+    public final static String EXTRA_MESSAGE = "br.eti.soares.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +40,13 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
